f5d33b267aeae2271024f61f1ca0f2bac50c7ffb
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-07-07 22:58:16 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars() = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+
+var movies = ["Lion King", "Lion King One and a Half", "Lion King 2"]
+function addElementToBeginningOfArray();
+    ["Lion King"...addElementToBeginningOfArray]
+}
+
+var places
+function addElementToEndOfArray = ["first", "second"] {
+
+}
